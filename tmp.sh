e7fcffd896050abbb3d9c33ea1966feac351bc1d
Renato Jareno <craickerjack@gmail.com>
2025-05-14 10:03:09 -0700

    initial commit

---
+++ tmp.sh
@@ -0,0 +1,18 @@
+mkdir css
+touch style.css
+touch index.html                 
+touch residents.html             
+touch family.html               
+touch compliance.html           
+touch reports.html               
+touch medication.html           
+touch billing.html             
+touch mealplanning.html       
+touch appointment.html       
+touch inventory.html        
+touch stafftraining.html        
+touch incidents.html           
+touch externalproviders.html  
+touch signage.html           
+touch admission.html        
+
